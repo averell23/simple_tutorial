4f5bafe06cf7c5010908faaf7715b11a00c52c19
Daniel Hahn <ghub@limitedcreativity.org>
2010-03-17 11:08:12 +0100

    Add #create and #destroy actions to the cars_controller, which will redirect back to the car's owner.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,25 @@
 class CarsController < ApplicationController
+  
+  # POST /cars
+  def create
+    @car = Car.new(params[:car])
+    
+    if @car.save
+      flash[:notice] = "You've got a new car."
+    else
+      flash[:notice] = "There's a problem with your new car."
+    end
+
+    redirect_to user_url(@car.user)
+  end
+
+  # DELETE /cars/1
+  def destroy
+    @car = Car.find(params[:id])
+    user = @car.user
+    @car.destroy
+
+    redirect_to user_url(user)
+  end
+  
 end
